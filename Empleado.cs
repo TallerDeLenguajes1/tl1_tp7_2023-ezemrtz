37c5da18a891130ee2d80f99d3d62ed85a110a19
“Ezequiel <ezequielmartinez2424@gmail.com>
2023-06-14 22:05:31 -0300

    Ejercicio 2 completado

--- Empleado.cs
+++ Empleado.cs
@@ -8,13 +8,76 @@ public enum Cargos{
     Investigador
 }
 public class Empleado{
-    string Nombre;
-    string Apellido;
-    DateTime FechaNac;
-    char EstadoCivil;
-    char Genero;
-    DateTime FechaIngreso;
-    double Sueldo;
-    Cargos Cargo;
+    private string nombre;
+    private string apellido;
+    private DateTime fechaNac;
+    private char estadoCivil;
+    private char genero;
+    private DateTime fechaIngreso;
+    private double sueldo;
+    private Cargos cargo;
 
-}
+    public string Nombre { get => nombre; set => nombre = value; }
+    public string Apellido { get => apellido; set => apellido = value; }
+    public DateTime FechaNac { get => fechaNac; set => fechaNac = value; }
+    public char EstadoCivil { get => estadoCivil; set => estadoCivil = value; }
+    public char Genero { get => genero; set => genero = value; }
+    public DateTime FechaIngreso { get => fechaIngreso; set => fechaIngreso = value; }
+    public double Sueldo { get => sueldo; set => sueldo = value; }
+    public Cargos Cargo { get => cargo; set => cargo = value; }
+
+    public int Antiguedad(){
+        int antiguedad = DateTime.Today.AddTicks(-FechaIngreso.Ticks).Year - 1;
+        return antiguedad;
+    }
+    public int Edad(){
+        int edad = DateTime.Today.AddTicks(-FechaNac.Ticks).Year - 1;
+        return edad;
+    }
+    public int Jubilacion(){
+        int jubil = 0;
+        if(Genero == 'F'){
+            if(Edad() < 60){
+                jubil = 60 - Edad();
+            }
+        }else{
+            if(Edad() < 65){
+                jubil = 65 - Edad();
+            }
+        }
+        return jubil;
+    }
+
+    public double Salario(){
+        double salario, adicional;
+        if(Antiguedad() <= 20){
+            adicional = Sueldo * Antiguedad() / 100;
+        }else{
+            adicional = Sueldo * 0.25;
+        }
+        if(Cargo == Cargos.Ingeniero | Cargo == Cargos.Especialista){
+            adicional += adicional * 0.5;
+        }
+        if(EstadoCivil == 'C'){
+            adicional += 15000;
+        }
+        salario = Sueldo + adicional;
+        return salario;
+    }
+    
+    public void Mostrar(){
+        Console.WriteLine("======== DATOS ========");
+        Console.WriteLine("Nombre: {0}", nombre);
+        Console.WriteLine("Apellido: {0}", apellido);
+        Console.WriteLine("Fecha de nacimiento: {0}", fechaNac.ToShortDateString());
+        Console.WriteLine("Estado Civil: {0}", estadoCivil);
+        Console.WriteLine("Genero: {0}", genero);
+        Console.WriteLine("Fecha de ingreso: {0}", fechaIngreso.ToShortDateString());
+        Console.WriteLine("Sueldo Basico: ${0}", sueldo);
+        Console.WriteLine("Cargo: {0}", cargo);
+        Console.WriteLine("Antiguedad: {0}", this.Antiguedad());
+        Console.WriteLine("Edad: {0}", this.Edad());
+        Console.WriteLine("Años para jubilarse: {0}", this.Jubilacion());
+        Console.WriteLine("Salario: ${0}", this.Salario());
+    }
+} 
--- Program.cs
+++ Program.cs
@@ -1,1 +1,52 @@
-﻿
+﻿using EspacioEmpleado;
+const int TAMA = 3;
+Empleado[] empleados = new Empleado[TAMA];
+int opcion;
+Console.WriteLine("======= Carga de datos =======");
+for (int i = 0; i < TAMA; i++){
+    empleados[i] = new Empleado();
+    Console.WriteLine($"-------- Empleado {i+1} -------");
+    Console.WriteLine("Ingrese el nombre: ");
+    empleados[i].Nombre = Console.ReadLine();
+    Console.WriteLine("Ingrese el apellido: ");
+    empleados[i].Apellido = Console.ReadLine(); 
+    Console.WriteLine("Ingrese la fecha de nacimiento (dd/mm/YYYY): ");
+    empleados[i].FechaNac = Convert.ToDateTime(Console.ReadLine()); 
+    Console.WriteLine("Ingrese el genero (F/M): ");
+    empleados[i].Genero = char.Parse(Console.ReadLine()); 
+    Console.WriteLine("Ingrese el estado civil (C/S): ");
+    empleados[i].EstadoCivil = char.Parse(Console.ReadLine()); 
+    Console.WriteLine("Ingrese la fecha de ingreso (dd/mm/YYYY): ");
+    empleados[i].FechaIngreso = Convert.ToDateTime(Console.ReadLine()); 
+    Console.WriteLine("Ingrese el sueldo basico: ");
+    empleados[i].Sueldo = double.Parse(Console.ReadLine());
+    Console.WriteLine("Ingrese el cargo: ");
+    Console.WriteLine("1.Auxiliar");
+    Console.WriteLine("2.Administrativo");
+    Console.WriteLine("3.Ingeniero");
+    Console.WriteLine("4.Especialista");
+    Console.WriteLine("5.Investigador");
+    do{
+        opcion = int.Parse(Console.ReadLine());
+    }while(opcion < 1 & opcion > 5);
+    empleados[i].Cargo = (Cargos)(opcion-1);
+}
+
+// d. Monto total
+double montoTotal = 0;
+for (int i = 0; i < TAMA; i++){
+    montoTotal += empleados[i].Salario();
+}
+
+// e. Mostrar mas proximo a jubilarse
+int edadJubilacion = empleados[0].Jubilacion(), indice = 0;
+for (int i = 1; i < TAMA; i++){
+    if(edadJubilacion > empleados[i].Jubilacion()){
+        edadJubilacion = empleados[i].Jubilacion();
+        indice = i;
+    }
+}
+Console.WriteLine("========== Empleado mas cercano a jubilarse ==========");
+empleados[indice].Mostrar();
+
+
